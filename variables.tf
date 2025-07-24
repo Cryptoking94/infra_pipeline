27199cf6c76d9e4e3a86e54f6b040aea3374a339
CLOPS_Muhammed_H <muhammed.hashir@cloudifyops.com>
2025-07-24 11:28:21 +0530

    Create the instance

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "ami_id" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
+  default     = "t3.large"
 }
 
 variable "instance_name" {
