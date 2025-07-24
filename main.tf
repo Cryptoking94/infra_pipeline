b27363b19a30dffff13ae3aff9ade0f5db4d4c12
CLOPS_Muhammed_H <muhammed.hashir@cloudifyops.com>
2025-07-24 08:46:25 +0530

    Test update to trigger validation workflow

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 # temp comment to trigger workflow
-#test
+#test ing
 module "vpc" {
   source              = "./modules/vpc"
   vpc_cidr            = var.vpc_cidr
